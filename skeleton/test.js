37bffca1b2985459b4ad0338b2e0773ced994e03
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-07 20:52:25 +0200

    add the jest and another file

---
+++ skeleton/test.js
@@ -0,0 +1,66 @@
+var logic = require('./logic');
+describe('go to hell',()=>{
+
+test("back to home",()=>{
+	expect(1).toBe(1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	test('Example test', function(t) {
+//   t.pass();
+//   t.end();
+// });
+})
+
+
+// describe('What component aspect are you testing?', function() {
+
+//     test('What should the feature do?', function () {
+// 		const actual = double(1);
+// 		const expected = 2;
+// 		expect(actual).toBe(expected);
+//     });
+
+//     test('What return when i diescribe 20', function () {
+// 		const actual = double(10);
+// 		const expected = 20;
+// 		expect(actual).toBe(expected);
+//     });
+    
+// });
