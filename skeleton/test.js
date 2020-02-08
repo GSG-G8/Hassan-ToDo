991f262847ca996187bdc00c2ec7db57a014dcb7
Hassan Al-najjar <ha77an19@gmail.com>
2020-02-08 22:23:47 +0200

    create mark Todo Function and make testing for it #11

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,6 +1,6 @@
 var logic = require("./logic");
 const todoFunctions = require("./logic");
-describe("go to hell", () => {
+describe("ToDo test function", () => {
   test(" addTodo function should return a new array with the new todo", () => {
     let actual = logic.addTodo([], { id: 1, b: "1" });
     let expected = [{ id: 1, b: "1" }];
@@ -30,6 +30,20 @@ describe("go to hell", () => {
     let todos = [{ id: 1, b: "1" }];
     let actual = logic.deleteTodo(todos, 1);
     let expected = [];
+    expect(actual).toEqual(expected);
+  });
+
+  test("markTodo function should toggle between done and not done", function() {
+    let todos = [{ id: 1, description: "test", done: false }];
+    let actual = logic.markTodo(todos, 1);
+    let expected = [{ id: 1, description: "test", done: true }];
+    expect(actual).toEqual(expected);
+  });
+
+  test("markTodo function should toggle between done and not done", function() {
+    let todos = [{ id: 2, description: "test", done: true }];
+    let actual = todoFunctions.markTodo(todos, 2);
+    let expected = [{ id: 2, description: "test", done: false }];
     expect(actual).toEqual(expected);
   });
 });
